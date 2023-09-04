4e1c2b2f686fb9933b6ddb9db7fe6c315f6722d4
pawel <pawelbudnik00@gmail.com>
2023-09-04 07:38:03 +0530

    first commit

---
+++ api/utils.py
@@ -0,0 +1,149 @@
+
+from rembg import remove
+import csv
+import os
+from django.conf import settings
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+import faiss
+import os
+import pickle
+from pathlib import Path
+
+class ImageSearch:
+    def __init__(self, dimension=2048):
+        self.dimension = dimension
+        print("Index Dimension:", dimension)  # Print the dimension for debugging
+        self.index = faiss.IndexFlatL2(dimension)
+        self.image_paths = []
+
+        # Initialize pre-trained ResNet model + higher level layers
+        self.model = models.resnet50(pretrained=True)
+
+        # Remove final fully connected layer to get 2048 dimensional output
+        modules = list(self.model.children())[:-2]
+        self.model = torch.nn.Sequential(*modules)
+        
+        if torch.cuda.is_available():
+            self.model = self.model.cuda()
+        
+        self.model.eval()
+
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+
+        ])
+    def save_all_index(self):
+        self.save_category_index("all")
+
+    def load_all_index(self):
+        if os.path.exists("all_faiss_index.pkl"):
+            self.load_category_index("all")
+        else:
+            self.create_empty_index()
+
+    def create_empty_index(self):
+        self.index = faiss.IndexFlatL2(self.dimension)
+        self.image_paths = []
+
+    def merge_category_into_all(self, category_name):
+        self.load_all_index()
+        category_searcher = ImageSearch()
+        category_searcher.load_category_index(category_name)
+        self.merge_with(category_searcher)
+        self.save_all_index()
+
+    def extract_features(self, image_path):
+        image = Image.open(image_path).convert("RGB")
+        tensor = self.transform(image).unsqueeze(0)
+        if torch.cuda.is_available():
+            tensor = tensor.cuda()
+        with torch.no_grad():
+            features = self.model(tensor)
+        
+        # Apply Global Average Pooling
+        features = features.mean([2, 3])
+        
+        return features.cpu().numpy()
+
+    def add_to_index(self, image_path):
+        print(image_path)
+        original_path = Path(image_path).resolve() 
+        image_path_relative = original_path.relative_to(settings.BASE_DIR)
+
+        processed_path = original_path.with_name(f"{original_path.stem}_processed{original_path.suffix}")
+
+        with open(image_path_relative, 'rb') as i:
+            with open(processed_path, 'wb') as o:
+                input = i.read()
+                output = remove(input)
+                o.write(output)
+        feature = self.extract_features(processed_path)
+        # print("Feature Shape:", feature.shape)  # Print the shape for debugging
+        self.index.add(feature)
+
+        self.image_paths.append(image_path)
+
+    def search(self, query_image_path, k=10):
+        query_feature = self.extract_features(query_image_path)
+        distances, indices = self.index.search(query_feature, k)
+
+        results = [self.image_paths[idx] for idx in indices[0]]
+        return results
+    def save_category_index(self, category_name):
+        index_path = f"{category_name}_faiss_index.pkl"
+        image_paths_path = f"{category_name}_image_paths.pkl"
+        faiss.write_index(self.index, index_path)
+        with open(image_paths_path, 'wb') as f:
+            pickle.dump(self.image_paths, f)
+
+    def load_category_index(self, category_name):
+        index_path = f"{category_name}_faiss_index.pkl"
+        image_paths_path = f"{category_name}_image_paths.pkl"
+        self.index = faiss.read_index(index_path)
+        with open(image_paths_path, 'rb') as f:
+            self.image_paths = pickle.load(f)   
+    def merge_with(self, another_searcher):
+        self.index.add(another_searcher.index.reconstruct_n(0, another_searcher.index.ntotal))
+        self.image_paths.extend(another_searcher.image_paths)
+
+def get_categories_from_csv():
+    categories = []
+    csv_path = os.path.join(settings.BASE_DIR, 'categories.csv')
+    with open(csv_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            categories.append(row[0])  # Assuming categories are in the first column
+
+    return categories
+
+def add_category_to_csv(category):
+    csv_path = os.path.join(settings.BASE_DIR, 'categories.csv')
+    with open(csv_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if category == row[0]:
+                return False  # Category already exists
+
+    # If not, add the category
+    with open('categories.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([category])
+
+    return True 
+
+def remove_category_from_csv(category):
+    # read the CSV, filter out the category and then write it back
+    with open('categories.csv', 'r') as f:
+        rows = [line.strip() for line in f.readlines() if line.strip() != category]
+
+    with open('categories.csv', 'w') as f:
+        for row in rows:
+            f.write(row + "\n")
+
+    return True
---
+++ api/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from django.core.files.storage import default_storage
+import os
+from django.conf import settings
+from django.core.files.base import ContentFile
+from .utils import *
+import json
+from pathlib import Path
+
+# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        categories = get_categories_from_csv()
+        return render(request, "home.html", {'categories': categories})
+
+class TrainView(View):
+    def get(self, request):
+        categories = get_categories_from_csv()
+        return render(request, "train.html", {'categories': categories})
+    
+class GetCategoryCountView(View):
+    def get(self, request):
+        base_path = os.path.join(settings.MEDIA_ROOT, 'ImageSearch', 'train_datasets')
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
+        base_path = os.path.join(settings.MEDIA_ROOT, 'ImageSearch', 'train_datasets')
+        categories = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
+        counts = {}
+        for category in categories:
+            count = len([f for f in os.listdir(os.path.join(base_path, category)) if os.path.isfile(os.path.join(base_path, category, f))])
+            counts[category] = count
+        return JsonResponse(counts)
+
+class AddCategoryView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        new_category = data.get('category', '')
+        print(new_category)
+        success = add_category_to_csv(new_category)
+
+        if success:
+            return JsonResponse({"success": True, "message": "Category added successfully"})
+        else:
+            return JsonResponse({"success": False, "message": "Failed to add category"}, status=400)
+        
+class DeleteCategoryView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            category_to_delete = data.get('category')
+            if not category_to_delete:
+                    return JsonResponse({'success': False, 'error': 'Category not provided'})
+            success = remove_category_from_csv(category_to_delete)
+
+            return JsonResponse({'success': success})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
+
+class TrainImageView(View):
+    def post(self, request):
+        searcher = ImageSearch()
+        models_dir = os.path.join(settings.BASE_DIR, 'models')
+        if not os.path.exists(models_dir):
+            os.makedirs(models_dir)
+        category = request.POST.get('categorySelect')
+        upload_files = request.FILES.getlist('files')
+        for upload_file in upload_files:
+            path = os.path.join(settings.MEDIA_ROOT, 'ImageSearch', 'train_datasets', category, upload_file.name)
+            os.makedirs(os.path.dirname(path), exist_ok=True)  # Create parent directories if they don't exist
+                    # If file with same name already exists, delete it
+            if default_storage.exists(path):
+                default_storage.delete(path)
+            default_storage.save(path, ContentFile(upload_file.read()))
+            searcher.add_to_index(path)
+            searcher.save_category_index(category)
+        searcher = ImageSearch()
+        searcher.merge_category_into_all(category)
+        return JsonResponse({"message": "success"})
+    
+class SearchView(View):
+    def post(self, request):
+        searcher = ImageSearch()
+        image = request.FILES.get('image')
+        category = request.POST.get('category')
+        print(category)
+                # Remove background from the uploaded image
+        img_data = image.read()
+        img_no_bg = remove(img_data)
+        path = os.path.join(settings.MEDIA_ROOT, 'uploads', image.name)
+        default_storage.save(path, ContentFile(img_no_bg))
+        
+        try:
+            searcher.load_category_index(category)
+            similar_images = searcher.search(path)
+            # Convert float scores to strings
+            similar_images_serializable = [img for img in similar_images]
+            print(similar_images_serializable)
+            response_data = {
+                "similar_images": similar_images_serializable
+            }
+            print(response_data)
+            return JsonResponse(response_data)
+        except Exception as e:
+            print(e)  # To see the exception in the console
+            response_data = {
+                "similar_images": []
+            }
+            return JsonResponse(response_data)
